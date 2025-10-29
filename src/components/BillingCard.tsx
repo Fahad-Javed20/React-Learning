0238b5b649784d07f1b5faa95312fecb9ca7f670
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-10-29 22:40:01 +0500

    Update BillingCard.tsx

--- src/components/BillingCard.tsx
+++ src/components/BillingCard.tsx
@@ -6,6 +6,7 @@ interface BillingCardProps {
 
 const BillingCard = ({ name, description, billAmount }: BillingCardProps) => {
   return (
+      
     <div className="grid w-230 grid-cols-2 gap-15 p-8 bg-white border rounded-2xl shadow-md items-center">
       <div className="space-y-3 text-left pl-10">
         <p className="font-bold text-lg text-transparent bg-clip-text bg-linear-to-r from-blue-400 via-red-400 to-purple-400">
@@ -25,7 +26,13 @@ const BillingCard = ({ name, description, billAmount }: BillingCardProps) => {
           What are team Users?
         </button>
       </div>
+
+
+
+    
     </div>
+
+    
   );
 };
 
